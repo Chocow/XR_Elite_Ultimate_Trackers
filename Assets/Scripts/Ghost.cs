33a9cf4c1adfe536b38ac79b15a575d0c35fe843
Antoine GALOPIN <galopin@et.esiea.fr>
2025-11-19 09:48:02 +0100

    C'est bien

--- Assets/Scripts/Ghost.cs
+++ Assets/Scripts/Ghost.cs
@@ -13,9 +13,19 @@ public class Ghost : MonoBehaviour
         m_HealthBar.GetComponent<HealthBar>().SetMaxHealth(m_MaxLife);
     }
 
+    private void Update()
+    {
+        if(m_CurrentLife <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Laser"))
+        Debug.Log("Pouic");
+
+        if (other.CompareTag("Laser"))
         {
             m_CurrentLife--;
             m_HealthBar.GetComponent<HealthBar>().SetHealth(m_CurrentLife);
